77e9a8337b8332107e7ff16cfa98195f40711714
7ingout <hadvjin@naver.com>
2022-08-09 17:43:44 +0900

    destinations CRUD 중 CR 까지 함

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const fs = require("fs")
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const dbinfo = fs.readFileSync('./database.json');
-const conf = JSON.parse(dbinfo)
+const conf = JSON.parse(dbinfo);
+const multer = require("multer");
 
 const connection = mysql.createConnection({
     host: conf.host,
@@ -18,6 +19,68 @@ const connection = mysql.createConnection({
 })
 app.use(express.json());
 app.use(cors());
+app.use("/upload", express.static("upload"));
+app.use("/upload2", express.static("upload"));
+
+
+// 파일 요청시 파일이 저장될 경로와 파일이름(요청된 원본파일이름) 지정
+const storage = multer.diskStorage({
+    destination:"./upload",
+    filename: function(req, file, cb) {
+        cb(null, file.originalname); // 원본 파일 이름과 똑같이 저장하겠다
+    }
+})
+
+// 업로드 객체
+const upload = multer({
+    storage: storage,
+    limits: { fieldSize: 1000000 }
+})
+
+// upload 경로로 post 요청이 왔을 경우 응답
+app.post("/upload", upload.single("cityImg"), function(req, res, next){
+    res.send({
+        cityImg: req.file.filename
+    })
+})
+app.post("/upload2", upload.single("cityMapImg"), function(req, res, next){
+    res.send({
+        cityMapImg: req.file.filename
+    })
+})
+
+// 국가 등록
+app.post('/addTrip', async(req,res) => {
+    const { cityImg, cityContinent, cityNational, cityDesc, cityDesc2, cityDesc3, cityMapImg} = req.body;
+    connection.query("insert into trip(`cityImg`, `cityContinent`, `cityNational`, `cityDesc`, `cityDesc2`, `cityDesc3`, `citymapImg`) values(?,?,?,?,?,?,?)",
+    [cityImg, cityContinent, cityNational, cityDesc, cityDesc2, cityDesc3, cityMapImg],
+    (err, result, fields) => {
+        res.send("등록되었습니다.");
+        console.log(err);
+    })
+})
+
+
+// gallery get 요청
+app.get("/trip", async (req, res)=> {
+    connection.query("select * from trip",
+    (err, result, fields)=> {
+        res.send(result)
+    })
+})
+
+// 상세보기
+app.get('/destinations/:cityNational', async (req,res)=>{
+    const params = req.params;
+    const { cityNational } = params;
+    connection.query(
+        `select * from trip where cityNational='${cityNational}'`,
+        (err, rows, fields)=>{
+            res.send(rows[0]);
+        }
+    )
+})
+
 
 // 회원가입 요청
 app.post("/join", async (req,res)=> {
@@ -122,21 +185,11 @@ app.get('/pics', async (req, res)=> {
     )
 })
 
-// 모든 장소 모아보기
-// destinations
-app.get('/trips', async (req, res)=> {
-    connection.query(
-        "select * from trip order by cityNational ASC",
-        (err, rows, fields)=> {
-            res.send(rows)
-        }
-    )
-})
-
-// 검색 기능
-app.get('/trips', async (req, res)=> {
+// 검색 기능 (나중에 구현하기)
+app.get('/search', async (req, res)=> {
     connection.query(
         `select * from trip where=`,
+        // where `{$_POST['search_m']}` Like '%{$_POST['search']}%'
         (err, rows, fields)=> {
             res.send(rows)
         }
@@ -145,5 +198,5 @@ app.get('/trips', async (req, res)=> {
 
 // 서버실행
 app.listen(port, () => {
-    console.log("고객 서버가 돌아가고 있습니다.")
+    console.log("서버가 돌아가고 있습니다.")
 })
